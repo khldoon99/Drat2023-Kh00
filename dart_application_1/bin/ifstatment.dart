e8a5ea0c6b3e1cba41cd2f8b62b5e5bf59bf0962
khldoon drawsheh <82982735+khldoon99@users.noreply.github.com>
2023-11-17 05:07:14 +0300

    add if and for loob

--- dart_application_1/bin/ifstatment.dart
+++ dart_application_1/bin/ifstatment.dart
@@ -4,95 +4,95 @@ void main() {
   // int c = 6;
 
   // print(a > b);
-  print('------' * 15);
+  // print('_______' * 15);
 
-  /// false
+  // // false
   // if (c > b) {
   //   print('c is greater than b');
   // } else {
   //   print('c is less than b');
   // }
 
-  // print('------' * 15);
+//   print('------' * 15);
 
-  /// Even or odd
-  int num = 6;
-  if (num % 2 == 0) {
-    print("Even");
-  } else {
-    print("Odd");
-  }
+//   /// Even or odd رقم زوجي او رقم فردي # مثال 
+  // int num = 6;
+  // if (num % 2 == 0) {
+  //   print("Even");
+  // } else {
+  //   print("Odd");
+  // }
+//   print('------' * 15);
 
-  print('------' * 15);
-  // print list
+//   // print list
 
-  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+  // List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 // insted  of using the normal print we can use something called for loop
 
-  for (var i = 0; i < numbers.length; i++) {
-    print(numbers[i]);
-  }
-
-  print('------' * 15);
-
-  for (var j = 0; j < 7; j++) {
-    print(j);
-  }
-
-  // inline if statement
-
-  int x = 10;
-  int y = 10;
-
-  print('------' * 15);
-
-  if (x > y) {
-    print('x is greater than y');
-  } else {
-    print('x is less than y');
-  }
-//        false >> 'x is less than y'
-  print('condition is true');
-
-  //         true >> 'x is greater than y'
-  //         false >> 'x is less than y'
-  var z = x > y ? 'x is greater than y' : 'x is less than y';
-  print(z);
-
-  //   true >> 'x is less than y'
-  //   false >> 'x is greater than y'
-  print('condition is false');
-  //  x> y is false >> 10 > 10 >> false >> we added ! to make it true
-  var n = !(x > y) ? 'x is greater than y' : 'x is less than y';
-  print(n);
-
-  print('------' * 15);
-
-  // loops in dart
-
-  // for loop
-
-  // while loop
-
-  // break and continue
-
-  // while (x < 10) {
-  //   print('hello');
-  //   x++;
-  //   if (x == 8) {
-  //     break;
-  //   }
+  // for (var i = 0; i < numbers.length; i++) {
+  //   print(numbers[i]);
   // }
 
-  print('------' * 15);
+//   print('------' * 15);
 
-  // do while loop
-// do while loop will run the code first and then check the condition
-  do {
-    print('hello');
-    print(x);
-    x++;
-  } while (x < 9);
+  // for (var j = 0; j < 7; j++) {
+  //   print(j);
+  // }
 
-  print('------' * 15);
+//   // inline if statement  
+
+//   int x = 10;
+//   int y = 10;
+
+//   print('------' * 15);
+
+//   if (x > y) {
+//     print('x is greater than y');
+//   } else {
+//     print('x is less than y');
+//   }
+// //        false >> 'x is less than y'
+//   print('condition is true');
+
+//   //         true >> 'x is greater than y'
+//   //         false >> 'x is less than y'
+//   var z = x > y ? 'x is greater than y' : 'x is less than y';
+//   print(z);
+
+//   //   true >> 'x is less than y'
+//   //   false >> 'x is greater than y'
+//   print('condition is false');
+//   //  x> y is false >> 10 > 10 >> false >> we added ! to make it true
+//   var n = !(x > y) ? 'x is greater than y' : 'x is less than y';
+//   print(n);
+
+//   print('------' * 15);
+
+//   // loops in dart
+
+//   // for loop
+
+//   // while loop
+
+//   // break and continue
+
+//   // while (x < 10) {
+//   //   print('hello');
+//   //   x++;
+//   //   if (x == 8) {
+//   //     break;
+//   //   }
+//   // }
+
+//   print('------' * 15);
+
+//   // do while loop
+// // do while loop will run the code first and then check the condition
+//   do {
+//     print('hello');
+//     print(x);
+//     x++;
+//   } while (x < 9);
+
+//   print('------' * 15);
 }
