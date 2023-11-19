9e7427fc3119cfc7a6f623acc0db617c0e24bde6
khldoon drawsheh <82982735+khldoon99@users.noreply.github.com>
2023-11-19 05:12:27 +0300

    add obb && classes

---
+++ dart_application_1/bin/calssAndObhects.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: file_names
+// void main(){
+
+// Cars().carInfo(brand:'toyota', model: 'corolla',year: 2019,color: 'red');
+// Cars().carInfo(color: 'black',year: 2020,brand: 'bmw',model: 'x6');
+// Cars().brandPrint('toyota');
+
+// }
+
+// يجب وضع ال class خارج void  واستدعاء ال class داخل ال void 
+
+
+// class Cars{
+//   String brand ='';
+//   String model = '';
+//   int year = 0;
+//   String color = '';
+
+//   void carInfo({brand,  model,  year,  color}){
+//     print('brand: $brand');
+//     print('model: $model');
+//     print('year: $year');
+//     print('color: $color');
+//   }
+//   void brandPrint(brand){
+//     print('brand: $brand');
+//   }
+// }
